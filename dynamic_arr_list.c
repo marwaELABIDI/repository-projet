e0bb63d419045de854414232dc08e405681aeb5c
marwaELABIDI <114148715+marwaELABIDI@users.noreply.github.com>
2023-03-20 23:01:46 +0000

    Update dynamic_arr_list.c

--- dynamic_arr_list.c
+++ dynamic_arr_list.c
@@ -29,3 +29,42 @@ int main() {
     list_append(&lst, 10);
     return 0;
 }
+//dynamic_array for char:
+#include<stdio.h>
+#include<stdlib.h>
+struct list
+{
+    char*arr;
+    int len;
+    int allocated;
+};
+void append(struct list *A,char elem){
+    if(A->len==A->allocated){
+        A->allocated*=2;
+          char*newarr=(char*)malloc(sizeof(char)*(A->allocated));
+          for(int i=0;i<A->len;i++){
+            newarr[i]=A->arr[i];
+          }
+          free(A->arr);
+          A->arr=newarr;
+    }
+    A->arr[A->len]=elem;
+    A->len+=1;
+}
+
+int main(){
+    struct list A;
+    A.len=0;
+    A.allocated=1;
+    A.arr=(char*)malloc(sizeof(char)*A.allocated);
+    append(&A,'M');
+    append(&A,'A');
+    append(&A,'R');
+    append(&A,'W');
+    append(&A,'A');
+    for(int i=0;i<A.len;i++){
+        printf("%c",A.arr[i]);
+    }
+    free(A.arr);
+    return 0;
+}
